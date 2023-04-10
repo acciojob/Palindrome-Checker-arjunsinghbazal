cbd903e3c7a9dc5677c44b442bb1bc6f0b5524a5
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-04-10 10:24:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+let Str=str.toLowerCase();
+Str.replaceAll(" ","");
+let i=0;
+let j=Str.length-1;
+while(i<j){
+	if(Str.charAt(i)!=Str.charAt(j)){
+		return false;
+	}
+	i++;
+	j--;
+}
+	return true;
 }
 module.exports = palindrome
